9b1867db9f50d43cc0df8b8709c3fc22b24bb27e
BryanFTucker <bryantucker@bryans-mbp.gateway.pace.com>
2014-07-15 08:37:03 -0500

    added an array example
    added an array example

---
+++ Values_and_Variables/array examlpe/js/script.js
@@ -0,0 +1,6 @@
+/**
+ * Created by bryantucker on 7/15/14.
+ */
+var carNames = ["Honda", "Toyota", "Lexus", "GMC", "Acura", "Scion"]
+
+console.log(carNames[4]) //This line tells the browser console to display Acura.
