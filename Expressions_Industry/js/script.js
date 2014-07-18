d4b82eb28abdca95c13fad676394cfea42d517c5
BryanFTucker <bryantucker@bryans-mbp.gateway.pace.com>
2014-07-18 16:01:56 -0500

    added variable
    added variable

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,4 +2,6 @@
  * Created by bryantucker on 7/17/14.
  * Expressions_Industry
  */
-prompt var("What is the width of you image?")
+prompt width = var("What is the width of you image?");
+
+Prompt height = var("What is the height of your image?");
