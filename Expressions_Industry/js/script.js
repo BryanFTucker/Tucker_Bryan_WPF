776e7845d3ff97181faec2051b9ae4bda59d3c87
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-18 19:59:42 -0500

    made width and height prompts more specific
    made width and height prompts more specific

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,9 +2,9 @@
  * Created by bryantucker on 7/17/14.
  * Expressions_Industry
  */
-var width = prompt("What is the width of you image?");
+var width = prompt("What is the width of you image in inches?");
 
-var height = prompt("What is the height of your image?");
+var height = prompt("What is the height of your image in inches?");
 
 var dpi = prompt("What is the image dpi?");
 
