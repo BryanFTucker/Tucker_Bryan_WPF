97c03cfab7c506e3765b8d71465cfe3bb365f828
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-19 16:49:12 -0500

    added width prompt
    added width prompt

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,4 +3,6 @@
  * Expressions_Industry
  * This JavaScript will calculate Dimensional weight. Dimensional weight is used by UPS to determine how much the customer is billed. I work at The UPS Store and I use this equation on a daily basis.
  */
-var length = prompt("What is the length of you package?")
+var length = prompt("What is the length of you package?");
+
+var width = prompt("What is the width of your package?");
