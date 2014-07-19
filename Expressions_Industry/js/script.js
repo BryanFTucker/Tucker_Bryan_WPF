61880195209fc4a0902b1822fe8100ac77360c68
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-19 17:22:14 -0500

    made prompts more descriptive
    made prompts more descriptive

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,11 +3,11 @@
  * Expressions_Industry
  * This JavaScript will calculate Dimensional weight. Dimensional weight is used by UPS to determine how much the customer is billed. I work at The UPS Store and I use this equation on a daily basis.
  */
-var length = prompt("What is the length of you package?");
+var length = prompt("What is the length of you package in inches?");
 
-var width = prompt("What is the width of your package?");
+var width = prompt("What is the width of your package in inches?");
 
-var height = prompt("What is the height of you package?");
+var height = prompt("What is the height of you package in inches?");
 
 const denominator = 166;
 
