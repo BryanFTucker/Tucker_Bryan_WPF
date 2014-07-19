7bf073521a8c88fb6b095f503ca939b4cb4de2e2
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-18 19:54:18 -0500

    corrected prompt syntax
    corrected prompt syntax

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,11 +2,11 @@
  * Created by bryantucker on 7/17/14.
  * Expressions_Industry
  */
-prompt width = var("What is the width of you image?");
+var width = prompt("What is the width of you image?");
 
-prompt height = var("What is the height of your image?");
+var height = prompt("What is the height of your image?");
 
-prompt dpi = var("What is the image dpi?");
+var dpi = prompt("What is the image dpi?");
 
 pxWidth = width * dpi;
 
