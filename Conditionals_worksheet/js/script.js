268768b1f6e1a1b52097739531b4581a8824b1b1
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-20 18:08:07 -0500

    moved parenthesis
    moved parenthesis

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -6,6 +6,6 @@ var userTemp = prompt("What is the numerical value of the temperature?");
 
 var tempScale = prompt("Which scale do you want to convert to? F or C?");
 
-result = (tempScale === "F") ? (9/5 * (userTemp + 32)) : (5/9 * (userTemp - 32));
+result = (tempScale === "F") ? (9/5) * (userTemp + 32) : (5/9) * (userTemp - 32);
 
 console.log(result);
