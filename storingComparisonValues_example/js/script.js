6d19a36b1b6dec4cd0afddd974e0167a83081799
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-20 13:52:17 -0500

    Beginning JavaScript 4th Edition
    Ch3, pg54

---
+++ storingComparisonValues_example/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by bryantucker on 7/20/14.
+ */
+var age = prompt("Enter your age: ", "");
+
+var isOverSixty = parseInt(age) > 60;
+
+    console.log("Older than 60: " + isOverSixty);
