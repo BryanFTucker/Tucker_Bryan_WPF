d64363df82068cda0b72fdabad6dc01df9a43f5a
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-19 19:55:06 -0500

    added var
    added var

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,3 +2,5 @@
  * Created by bryantucker on 7/18/14.
  * Expressions_Wacky
  */
+var coolCars = ["Audi", "Aston Martin", "Ferrari", "Bently", "Lamborghini", "Maserati", "Rolls-Royce", "Morgan", "McLaren", "Lexus"];
+
