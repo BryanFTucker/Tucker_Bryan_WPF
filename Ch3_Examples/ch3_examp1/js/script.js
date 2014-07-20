03891063f79d59a3eb4dbab7b1c6a3ce188236ed
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-20 14:57:28 -0500

    if statement example
    if statement example

---
+++ Ch3_Examples/ch3_examp1/js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by bryantucker on 7/20/14.
+ * ch3_examp1, pg55
+ */
+var degFahren = Number(prompt("Enter the degrees Fahrenheit",32));
+var degCent;
+
+degCent = 5/9 * (degFahren - 32);
+
+console.log(degFahren + "\xB0 Fahrenheit is " + degCent + "\xB0 centigrade<br />");
+
+if (degCent < 0)
+{
+    console.log("That's below the freezing point of water");
+}
+
+if (degCent == 100)
+{
+    console.log("That's the boiling point of water");
+}
