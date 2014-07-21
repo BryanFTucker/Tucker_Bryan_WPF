d9aec158010876235fcf9f29d8ab24de5fba68d7
BryanFTucker <bryantucker@bryans-mbp.gateway.pace.com>
2014-07-21 17:23:24 -0500

    created and moved to group 1 directory
    created and moved to group 1 directory

--- Conditionals_worksheet/Group1/js/script.js
+++ Conditionals_worksheet/Group1/js/script.js
@@ -5,7 +5,7 @@
 var userTemp = prompt("What is the numerical value of the temperature?");
 
 var tempScale = prompt("Which scale do you want to convert to? F or C?");
-
+    
 //result = (tempScale === "F") ? 9/5 * (userTemp + 32) : 5/9 * (userTemp - 32);
 
 if (tempScale === "F"){
