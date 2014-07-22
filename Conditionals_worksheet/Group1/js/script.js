c70f25831f747d37ad3b2a0d96280c9a2c80acb5
BryanFTucker <bryantucker@bryans-mbp.gateway.pace.com>
2014-07-22 08:41:21 -0500

    added comments
    added comments

--- Conditionals_worksheet/Group1/js/script.js
+++ Conditionals_worksheet/Group1/js/script.js
@@ -6,8 +6,10 @@ var userTemp = prompt("What is the numerical value of the temperature?");
 
 var tempScale = prompt("Which scale do you want to convert to? F or C?");
 
+    //This ternary operator will calculate Fahrenheit if the user enters "F." If the user enters "C" it will do the other statement.
 result = (tempScale === "F") ? (9/5 * userTemp) + 32 : 5/9 * (userTemp - 32);
 
+    //This "if - else" chooses which alert to display based on user input.
 if (tempScale === "F"){
     alert("The temperature is " + result + "\xB0 Fahrenheit.")
 }else{
