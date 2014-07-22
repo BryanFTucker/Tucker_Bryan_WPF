1ac8dc47d51b4a0819f14eee7cc526f6228e1d46
BryanFTucker <bryantucker@bryans-mbp.gateway.pace.com>
2014-07-22 11:24:11 -0500

    added final alert
    added final alert

--- Conditionals_worksheet/Group2/js/script.js
+++ Conditionals_worksheet/Group2/js/script.js
@@ -3,15 +3,19 @@
  * Group 2 - Check the Login
  */
 //The correct username is TheUPSStore. The correct password is password.
-const userName = "TheUPSStore"
-const passWord = "password"
+const userName = "TheUPSStore";
+const passWord = "password";
 
 var inputUserName = prompt("Please enter your username:");
 
 var inputPassWord = prompt("Please enter your password:");
 
 if (inputUserName === userName && inputPassWord === passWord){
-    alert("Welcome, " + userName + "!");
+   result = alert("Welcome, " + userName + "!");
 }else if (inputUserName != userName){
-    alert("User not found. Try again.")
+   result = alert("User not found. Try again.");
+}else {
+   result = alert("Password does match our records.");
 }
+
+alert(result);
