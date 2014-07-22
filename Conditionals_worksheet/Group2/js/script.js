40987593a941923731eba17ac03bcf1e2c42a424
BryanFTucker <bryantucker@bryans-mbp.gateway.pace.com>
2014-07-22 09:00:38 -0500

    set up directories and added const
    set up directories and added const

---
+++ Conditionals_worksheet/Group2/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by bryantucker on 7/22/14.
+ * Group 2 - Check the Login
+ */
+//The correct username is TheUPSStore. The correct password is password.
+const userName = "TheUPSStore"
+const passWord = "password"
+
