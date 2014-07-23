838c1069d4d0a5ca2b1e60d3867a6bdbbe99c0b7
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-23 11:22:55 -0500

    created tirePressure array
    created tirePressure array

--- Conditionals_worksheet/Group3/js/script.js
+++ Conditionals_worksheet/Group3/js/script.js
@@ -2,3 +2,6 @@
  * Created by bryantucker on 7/22/14.
  * Group 3: Multiple Conditions, Tire Pressure
  */
+//Pressure for each tire in PSI. Order as shown: FL, FR, RL, RR.
+tirePressure = [30, 30, 32, 32];
+
