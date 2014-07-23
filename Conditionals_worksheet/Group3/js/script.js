8097bc7eff42834eb7d6b294795e39eada5ea62a
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-23 13:17:11 -0500

    changed conditional
    changed conditional

--- Conditionals_worksheet/Group3/js/script.js
+++ Conditionals_worksheet/Group3/js/script.js
@@ -5,3 +5,9 @@
 //Pressure for each tire in PSI. Order as shown: FL, FR, RL, RR.
 var tirePressure = [30, 30, 32, 32];
 
+if (tirePressure[0] === tirePressure[1] && tirePressure[2] === tirePressure[3]){
+    alert("The Tires pass spec!");
+}else{
+    alert("Get your tires checked out!");
+}
+
