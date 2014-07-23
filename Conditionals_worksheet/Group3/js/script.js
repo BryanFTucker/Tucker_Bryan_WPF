79484a54c9e357a856064ab9a6587b544c408a23
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-23 13:02:56 -0500

    deleted multi-array
    deleted multi-array

--- Conditionals_worksheet/Group3/js/script.js
+++ Conditionals_worksheet/Group3/js/script.js
@@ -3,13 +3,5 @@
  * Group 3: Multiple Conditions, Tire Pressure
  */
 //Pressure for each tire in PSI. Order as shown: FL, FR, RL, RR.
-var tirePressure = [];
-
-tirePressure[0] = [];
-tirePressure [0][0] = 30;
-tirePressure [0][1] = 30;
-
-tirePressure[1] = [];
-tirePressure [1][0] = 32;
-tirePressure [1][1] = 32;
+var tirePressure = [30, 30, 32, 32];
 
