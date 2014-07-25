57918ebf1b4a34b7040920f756852006faeb17f5
BryanFTucker <bryantucker@bryans-mbp.gateway.pace.com>
2014-07-25 17:46:15 -0500

    created conditionals_industry
    created conditionals_industry

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by bryantucker on 7/25/14.
+ * Conditionals_Industry
+ */
+//Is the customer's item fragile?
+var fraGile = prompt("Is you package fragile?");
+
+if (fraGile === "") {
+    prompt("Please enter yes or no");
+}
