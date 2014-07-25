be0cc7e90f1ff88eaeb0bca43b653bf8785e468d
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-24 19:38:35 -0500

    added label to time of day prompt
    added label to time of day prompt

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,16 +4,18 @@
  * Conditionals_Personal
  */
 //Ask the user what time of day it is. Morning, afternoon, or evening.
-var timeOfDay = prompt("Is it morning, afternoon, or evening?");
+var timeOfDay = prompt("What time of day is it? ", "morning, afternoon, or evening?");
 
 //Ask the user if the dogs have food.
 var foodForDogs = prompt("Do the dogs have food?");
 
-//If it's morning or the dogs don't have food then they need to be be feed. If it any other time of day rather than morning, the dogs do not get feed, unless they don't have food.
+//If it is morning then the dogs need food.
 if (timeOfDay === "morning"){
     console.log("It's time to feed the dogs.");
+//If the dogs do not have food, then they need food.
 }else if (foodForDogs === "no"){
     console.log("It's time to feed the dogs.");
+//If the dogs have food and the time of day is afternoon or evening, then they do not need food.
 }else{
     console.log("The dogs don't need food.");
 }
