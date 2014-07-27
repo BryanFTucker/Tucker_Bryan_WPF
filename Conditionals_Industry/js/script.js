437ed136f40cb56034a6c28e2eb60f0df6cc4424
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-26 21:29:48 -0500

    added js comment
    added js comment

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,7 +1,7 @@
 /**
  * Created by bryantucker on 7/25/14.
  * Conditionals_Industry
- *
+ *The Fragile Package
  */
 //Is the customer's item fragile?
 var fraGile = prompt("Is you package fragile?");
