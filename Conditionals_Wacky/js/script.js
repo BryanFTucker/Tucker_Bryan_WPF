b5f7cb07a7b54863e159f2082ca4fabf6b3b6764
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-27 18:16:46 -0500

    added second variable
    added second variable

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -7,3 +7,7 @@ var lightSpeed = prompt("Shall we prepare the ship for light speed?", "yes or no
 
 lightSpeed === "" ? prompt("Please enter yes or no.") : lightSpeed;
 
+var shipSpeed = prompt("How fast do you want to go?", "warp 1, warp 2, or warp 3");
+
+shipSpeed === "" ? prompt("Please enter a velocity", "warp 1, warp 2, warp 3"): shipSpeed;
+
