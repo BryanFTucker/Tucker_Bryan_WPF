8abba2b50168ddeeeb43f19725f8e6fa1b094195
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-27 21:52:51 -0500

    added comments
    added comments

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,27 +3,37 @@
  * Conditionals_Wacky
  * Spaceballs Speed Test
  */
+//input variable from user
 var lightSpeed = prompt("Shall we prepare the ship for light speed?");
 
+//test variable for proper input
 if (lightSpeed === ""){
+    //ask the user to enter yes or no
     prompt("Please enter yes or no");
+//If the user does not want to go to light speed then a message will display in the console
 }else if (lightSpeed === "no"){
+    //displays message in the browser console
     console.log("It's going to take us a while to get there.");
 }
-
+//input second variable from user
 var shipSpeed = prompt("How fast do you want to go?", "1, 2, 3");
 
+//test user input. input cannot be equal to zero or greater than three.
 if (shipSpeed == 0 || shipSpeed > 3){
+    //asks user to input a correct value and assigns user input to test variable after test is completed
     var shipSpeed = prompt("Please enter a velocity", "1, 2, 3");
 }
 
-
+//if user picks a velocity of 1 this message will display
 if (shipSpeed === "1" || shipSpeed === 1){
     console.log("You're going faster than the speed of light!");
+//if user picks a velocity of 2 this message will display
 }else if(shipSpeed === "2" || shipSpeed === 2){
     console.log("You're going twice the speed of light. That's Ridiculous speed!");
+//if user picks a velocity of 3 this message will display
 }else if (shipSpeed === "3" || shipSpeed === 3){
     console.log("That's Ludicrous speed! You've gone into plaid!");
+//if user enters anything else then a final message will display
 }else{
    console.log("Maybe you should install some secret hyper jets!");
 }
