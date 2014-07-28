6d11fb8e75b41d0089fd3b933b8b86a2eb181e17
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-27 20:35:23 -0500

    added comparison operator
    added comparison operator

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -11,11 +11,18 @@ if (lightSpeed === ""){
     console.log("It's going to take us a while to get there.");
 }
 
+var shipSpeed = prompt("How fast do you want to go?", "1, 2, 3");
 
-var shipSpeed = prompt("How fast do you want to go?", "warp 1, warp 2, or warp 3");
-
-if (shipSpeed === ""){
-    prompt("Please enter a velocity", "warp 1, warp 2, warp 3");
+if (shipSpeed === "" || (shipSpeed <= 0 && shipSpeed > 3)){
+    prompt("Please enter a velocity", "1, 2, 3");
 }
+
+
+var v1 = 1
+
+var v2 = 2
+
+var v3 = 3
+
 
 
