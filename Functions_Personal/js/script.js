79a20fca02c02baf61ea502c310bd9259365a2c5
BryanFTucker <bryantucker@bryans-mbp.gateway.pace.com>
2014-07-31 13:35:22 -0500

    create directory and initialize variables
    create directory and initialize variables

---
+++ Functions_Personal/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by bryantucker on 7/31/14.
+ * Functions_Personal
+ * Driving Cost Calculator
+ */
+
+var distance = prompt("How far are you driving?");
+
+var milePerGallon = prompt("What is the average MPG for your vehicle?");
+
+var gasCost = prompt("How much does gas cost in your area?");
