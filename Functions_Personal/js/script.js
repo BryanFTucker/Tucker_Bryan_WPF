2ffe79109b00b3defe1851d0763b6fcbc74ef16b
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-31 18:04:40 -0500

    added while loop test for user prompt
    added while loop test for user prompt

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,8 +4,19 @@
  * Driving Cost Calculator
  */
 
-var distance = prompt("How far are you driving?");
+var distance = prompt("How many miles are you driving?");
 
+    while (distance === "" || isNaN(distance)){
+        distance = prompt("Please enter a number?");
+    }
 var milePerGallon = prompt("What is the average MPG for your vehicle?");
 
+    while (milePerGallon === "" || isNaN(milePerGallon)){
+        milePerGallon = prompt("Please enter a number?");
+    }
+
 var gasCost = prompt("How much does gas cost in your area?");
+
+    while (gasCost === "" || isNaN(gasCost)){
+        gasCost = prompt("Please enter a number?");
+    }   
