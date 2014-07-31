d8c983b21610f126e2e6b415c943535d3432ad30
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-07-30 23:26:53 -0500

    function setup complete
    function setup complete

--- Functions_Worksheet/Calculate_the_Circumference/js/script.js
+++ Functions_Worksheet/Calculate_the_Circumference/js/script.js
@@ -7,3 +7,16 @@ var radius = prompt("What is the radius of the circle?");
 while (isNaN(radius) || radius === ""){
     radius = prompt("Please enter a number for the radius.");
 }
+
+const pi = 3.14;
+
+function circleLenght(r){
+
+    var formula = pi * (Math.pow(r,2));
+
+    return formula;
+}
+
+var results = circleLenght(radius);
+
+console.log("The circumference is " + results);
