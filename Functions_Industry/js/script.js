8e1bbda57d2bc5dc4eec9bab705cea140d5655ec
BryanFTucker <bryantucker@Bryans-MacBook-Pro.local>
2014-08-03 16:38:51 -0500

    added variable, variable test while loop, and if conditional
    added variable, variable test while loop, and if conditional

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,14 @@
 /**
  * Created by bryantucker on 8/3/14.
+ * UPS Package Insurance
  */
+
+var insurance = prompt("Does your package need additional insurance?");
+
+    while (insurance != "yes" && insurance === ""){
+        insurance = prompt("Please enter yes or no.");
+    }
+
+    if (insurance != "yes"){
+        console.log("Since you don't need additional insurance, have a nice day.")
+    }
